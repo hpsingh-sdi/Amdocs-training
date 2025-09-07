7d5653d6c1cb6310057af7e34b532a46d94ef24e
hpsingh-sdi <punjabicoder870@gmail.com>
2025-09-07 11:50:18 +0530

    Update and rename Python Assignment to Python_1.py

--- Python_1.py
+++ Python_1.py
@@ -2,46 +2,58 @@ Python Assignment
 
 ######Tuples######
 
+#1
 product = ("Laptop",50000,"Black",'Samsung','Electronics')
 print("Products : ",product)
 
+#2
 print(product[1])   #second element in product tuple
 
+#3
 result = product[-2:]   #Last two elements
 print(result)
 
+#4
 if "Electronics" in product:
     print("Present")
 else:
     print("Not Present")
 
+#5
 prices = (1000,1500,1200,1200,1100,900)
 print(prices.count(1000))
 
+#6
 min = min(prices)
 max = max(prices)
 print("Min : ",min," Max : ",max)
 
+#7
 for item in product:
     print(item)
 
+#8
 new_product = list(product)
 new_product[1]=55000
 product = tuple(new_product)
 print(product)
 
+#9
 tuple2 = ('In Stock',)
 product = product+tuple2
 print(product)
 
+#10
 new_product = list(product)
 new_product.remove("Electronics")
 product = tuple(new_product)
 print(product)
 
+#11
 a,b,*rest = product
 print(a,b,*rest)
 
+#12
 prod1 = ('iPhone',50000)
 prod2 = ('TV',100000)
 prod3 = ('Fridge',30000)
